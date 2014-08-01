458806a7174e74e7fbeb95e2c577e917e859449f
Devanshu arora <devanshu@vinsol.com>
2014-08-01 11:42:03 +0530

    Customer account balance exercise

---
+++ customer_account/bin/main.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/customer.rb'
+
+puts 'Enter the name of the account holder'
+customer = Customer.new(gets.chomp)
+puts customer
+puts 'Enter the amount you want to deposit'
+customer.deposit(gets.chomp.to_f)
+puts customer
+puts 'Enter the amount you want to withdraw'
+customer.withdraw(gets.chomp.to_f)
+puts customer
