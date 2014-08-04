175e9ac341fec862df2f37282244b40665c30601
Devanshu arora <devanshu@vinsol.com>
2014-08-04 15:19:27 +0530

    fix name issue

--- array_hash/lib/array.rb
+++ array_hash/lib/array.rb
@@ -3,9 +3,9 @@ class Array
   def group_by_length
     hash_to_store = Hash.new { |hash, key| hash[key] = [] }
     for element in self do
-      new_hash[element.to_s.size] << element
+      hash_to_store[element.to_s.size] << element
     end
-    new_hash
+    hash_to_store
   end
 end
 
