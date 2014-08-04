961ae1de62734026560c746cda1995d7a371ff80
Devanshu arora <devanshu@vinsol.com>
2014-08-04 11:04:51 +0530

    fix naming issue power_array exercise

--- power_array/bin/main.rb
+++ power_array/bin/main.rb
@@ -1,16 +1,4 @@
 require_relative '../lib/array'
 
-puts 'Enter the size of array'
-size = gets.chomp.to_i
-puts 'Enter the array elements'
-array = Array.new(size)
-for i in (0...size)
-array[i] = gets.chomp.to_i
-end
-puts'Entered array is'
-for i in (0...size)
-puts array[i]
-end
-puts 'Enter the power'
-puts "\n#{ array.power(gets.chomp.to_i) }"
+print [1, 2, 3, 4, 5, 6].power(3)
 
