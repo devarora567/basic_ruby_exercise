22eb80075956a52245223e3026622b8fc34ddaf3
Devanshu arora <devanshu@vinsol.com>
2014-08-06 12:55:43 +0530

    fix spacing issues

--- fibonacci_series/lib/series.rb
+++ fibonacci_series/lib/series.rb
@@ -1,7 +1,7 @@
 class Series
 
   def fibonacci
-    operand1,  operand2 = 0, 1
+    operand1, operand2 = 0, 1
     while operand1 <= 1000
       yield operand1
       operand1, operand2 = operand2, operand1 + operand2
