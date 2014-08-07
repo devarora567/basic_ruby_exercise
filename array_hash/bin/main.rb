c3c73b6a01086117d626fb3a4755850789cd1ffd
Devanshu arora <devanshu@vinsol.com>
2014-08-07 17:03:28 +0530

    fix logic issues

--- array_hash/bin/main.rb
+++ array_hash/bin/main.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/array'
 
-puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ'].flatten.group_by_length
+puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ', { a: 1, b: 2 }, [1, 2, 3]].group_by_length
--- array_hash/lib/array.rb
+++ array_hash/lib/array.rb
@@ -3,7 +3,11 @@ class Array
   def group_by_length
     hash_to_store = Hash.new { |hash, key| hash[key] = [] }
     for element in self
-      hash_to_store[element.to_s.size] << element
+      if element.class == Fixnum
+        hash_to_store[element.to_s.size] << element
+      else
+        hash_to_store[element.size] << element
+      end
     end
     hash_to_store
   end
