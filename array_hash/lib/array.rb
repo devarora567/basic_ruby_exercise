c7a568869b9a2a24d47f0ed8f901313a7d685816
Devanshu arora <devanshu@vinsol.com>
2014-08-08 12:47:28 +0530

    fix optimization issues

--- array_hash/lib/array.rb
+++ array_hash/lib/array.rb
@@ -3,11 +3,7 @@ class Array
   def group_by_length
     hash_to_store = Hash.new { |hash, key| hash[key] = [] }
     for element in self
-      if element.class == Fixnum
-        hash_to_store[element.to_s.size] << element
-      else
-        hash_to_store[element.size] << element
-      end
+      element.class == Fixnum ? hash_to_store[element.to_s.size] << element : hash_to_store[element.size] << element
     end
     hash_to_store
   end
