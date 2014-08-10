3dad24cca2dd2b0a0a50c31eb5a70cab38809704
Devanshu arora <devanshu@vinsol.com>
2014-08-11 00:22:30 +0530

    fix issues

--- character_by_type/lib/string.rb
+++ character_by_type/lib/string.rb
@@ -1,15 +1,16 @@
 class String
 
+  LOWER_CASE = 'a'..'z'
+  UPPER_CASE= 'A'..'Z'
+  DIGIT = '0'..'9'
+
   def count_character_by_type
-    lower_case = 'a'..'z'
-    upper_case = 'A'..'Z'
-    digit = '0'..'9'
     char_count= Hash.new(0)
     each_char do |character|
       case character
-      when lower_case then char_count[:lowercase] += 1
-      when upper_case then char_count[:uppercase] += 1
-      when digit then char_count[:digit] += 1
+      when LOWER_CASE then char_count[:lowercase] += 1
+      when UPPER_CASE then char_count[:uppercase] += 1
+      when DIGIT then char_count[:digit] += 1
       else char_count[:special_characters] += 1
       end
     end
